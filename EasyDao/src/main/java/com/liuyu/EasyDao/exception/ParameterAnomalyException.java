de3f2877da3f3cb7f7405df739a5851f700fbe52
liuyu <liuyu_private_work@163.com>
2015-01-15 14:38:04 +0800

    自定义异常

---
+++ EasyDao/src/main/java/com/liuyu/EasyDao/exception/ParameterAnomalyException.java
@@ -0,0 +1,14 @@
+package com.liuyu.EasyDao.exception;
+/**参数个数与传值个数不匹配异常*/
+public class ParameterAnomalyException extends Exception {
+	private static final long serialVersionUID = -4724927453901654587L;
+
+	public ParameterAnomalyException() {
+		super();
+	}
+
+	public ParameterAnomalyException(String message) {
+		super(message);
+	}
+
+}
