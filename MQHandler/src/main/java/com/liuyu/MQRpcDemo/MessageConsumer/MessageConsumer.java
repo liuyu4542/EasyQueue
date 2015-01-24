af3d3abbe2d1172986c8f1187dc858ca00bc54d2
liuyu <liuyu_private_work@163.com>
2015-01-24 18:04:46 +0800

    修改消息处理类中的处理方法为静态方法

--- MQHandler/src/main/java/com/liuyu/MQRpcDemo/MessageConsumer/MessageConsumer.java
+++ MQHandler/src/main/java/com/liuyu/MQRpcDemo/MessageConsumer/MessageConsumer.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import com.liuyu.MQRpcDemo.Factory.AMQPFactory;
 public class MessageConsumer {
-	public String recvMessage() {
+	public static String recvMessage() {
 		ApplicationContext context = new AnnotationConfigApplicationContext(AMQPFactory.class);
 		AmqpTemplate amqpTemplate = context.getBean(AmqpTemplate.class);
 		return amqpTemplate.receiveAndConvert().toString();
