403ec2703eaed7fa0d82c3d1841d76218db7d08e
liuyu <liuyu_private_work@163.com>
2015-01-24 18:05:40 +0800

    修改消息发送方法为静态方法

--- MQHandler/src/main/java/com/liuyu/MQRpcDemo/MessageProducer/MessageProducer.java
+++ MQHandler/src/main/java/com/liuyu/MQRpcDemo/MessageProducer/MessageProducer.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.liuyu.MQRpcDemo.Factory.AMQPFactory;
 /**消息发送*/
 public class MessageProducer {
-	public void sendMessage(String message){
+	public static void sendMessage(String message){
 		ApplicationContext context = new AnnotationConfigApplicationContext(AMQPFactory.class);
 		AmqpTemplate amqpTemplate = context.getBean(AmqpTemplate.class);
 		amqpTemplate.convertAndSend(message);
