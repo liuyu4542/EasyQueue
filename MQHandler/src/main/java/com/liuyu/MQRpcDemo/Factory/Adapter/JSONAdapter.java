6135c6073b0243112924ceec06904ecdd15cc7ad
liuyu <liuyu_private_work@163.com>
2015-01-25 14:50:27 +0800

    增加转换

---
+++ MQHandler/src/main/java/com/liuyu/MQRpcDemo/Factory/Adapter/JSONAdapter.java
@@ -0,0 +1,20 @@
+package com.liuyu.MQRpcDemo.Factory.Adapter;
+import java.util.HashMap;
+import java.util.List;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+/**类型转换器*/
+public class JSONAdapter {
+	public static List<HashMap<String, Object>> getListType(String message){
+		return  JSON.parseObject(message,new TypeReference<List<HashMap<String,Object>>>(){});
+	}
+	public static String parseListType(List<HashMap<String, Object>> list){
+		return  JSON.toJSONString(list);
+	}
+	public static HashMap<String, Object> getHashMapType(String message){
+		return  JSON.parseObject(message,new TypeReference<HashMap<String,Object>>(){});
+	}
+	public static String parseHashMapType(HashMap<String, Object> map){
+		return  JSON.toJSONString(map);
+	}
+}
